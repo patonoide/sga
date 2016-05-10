6fb6937a785615298f7f94271925064c2d7b6fd9
Rafael Fernandes <rafaelalves@cjr.org.br>
2016-05-10 11:09:43 -0300

    Incluindo assets

--- config/application.rb
+++ config/application.rb
@@ -29,6 +29,8 @@ module SistemaDeAtas
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :'pt-BR'
     config.assets.precompile += Ckeditor.assets
+    config.assets.precompile += %w( ckeditor/* )
+    config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   end
