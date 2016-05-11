d31d30405fa4e0fc4de8622cca2008d89428389b
Rafael Fernandes <rafaelalves@cjr.org.br>
2016-05-10 23:51:24 -0300

    Envia emails em producao 2.0

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,5 +77,19 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  Rails.application.routes.default_url_options[:host] = 'http://sga-cjr.herokuapp.com'
+  config.action_mailer.default_url_options = { host: 'sga-cjr.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default charset: "utf-8"
+
+  ActionMailer::Base.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    authentication: :plain,
+    domain: 'gmail.com',
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
+  }
+
 end
