17dd9965d58415248d701805dcf25979960dc09a
Gabriel Porto Oliveira <gabrielpoliveira@ig.com.br>
2018-08-29 12:32:56 -0300

    Just to make sure email is being sent

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,8 +2,7 @@ class UserMailer < ApplicationMailer
     default from: 'faltas@cjr.org.br'
 
   def welcome_email
-    @user = params[:user]
-    @url  = 'http://example.com/login'
-    mail(to: email do diogo, subject: 'Falta ou Atraso')
+    
+    mail(to: 'gabrielporto@cjr.org.br', subject: 'Falta ou Atraso')
   end
 end
